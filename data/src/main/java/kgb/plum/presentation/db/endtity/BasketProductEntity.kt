c798671900cdfe23f6e7e2726e39337e78a35285
KimGeunBeom <59364681+agfalcon@users.noreply.github.com>
2023-05-18 16:21:18 +0900

    [2023.05.18] (room) room gradle 설정 및 entity 작성

---
+++ data/src/main/java/kgb/plum/presentation/db/endtity/BasketProductEntity.kt
@@ -0,0 +1,16 @@
+package kgb.plum.presentation.db.endtity
+
+import kgb.plum.domain.model.Category
+import kgb.plum.domain.model.Price
+import kgb.plum.domain.model.Shop
+
+data class BasketProductEntity(
+    val productId: String,
+    val productName: String,
+    val imageUrl: String,
+    val price: Price,
+    val category: Category,
+    val shop: Shop,
+    val isNew: Boolean,
+    val isFreeShipping: Boolean,
+)
